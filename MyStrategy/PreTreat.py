f8887068559e807aa0f904ab0e7cc0c8e98b35bb
liuliu <183741946@qq.com>
2023-09-06 16:23:58 +0800

    2023/09/06: 1.选取热力图显示的相关性最高的几个维度数据来建训练数据集

---
+++ MyStrategy/PreTreat.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+import datetime
+import os
+import akshare as ak
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import PrepairData as prd
+
+WIN_ENV_FLAG = True
+FILEDIR = "stocks"
+STOCK_INFO_FILE = "text.txt"
+
+
+class PreTreadData:
+
+    def __init__(self, df):
+        data_set = df
+        print("PreTread Class init:")
+
+    # 依据特征重要性，选择low high open来进行预测close
+    # 数据选择t-n, ...., t-2 t-1 与 t 来预测未来 t+1
+    # 转换原始数据为新的特征列来进行预测,time_window可以用来调试用前几次的数据来预测
+
+    def series_to_supervised(self, data, time_window=3):
+        data_columns = ['open', 'high', 'low', 'close']
+        data = data[data_columns]  # Note this is important to the important feature choice
+        cols, names = list(), list()
+        for i in range(time_window, -1, -1):
+            # get the data
+            cols.append(data.shift(i))  # 数据偏移量
+
+            # get the column name
+            if (i - 1) <= 0:
+                suffix = '(t+%d)' % abs(i - 1)
+            else:
+                suffix = '(t-%d)' % (i - 1)
+            names += [(colname + suffix) for colname in data_columns]
+
+        # concat the cols into one dataframe
+        agg = pd.concat(cols, axis=1)
+        agg.columns = names
+        agg.index = data.index.copy()
+        # remove the nan value which is caused by pandas.shift
+        agg = agg.dropna(inplace=False)
+
+        # remove unused col (only keep the "close" fied for the t+1 period)
+        # Note col "close" place in the columns
+
+        len_ = len(data_columns) * time_window
+        col_numbers_drop = []
+        for i in range(len(data_columns) - 1):
+            col_numbers_drop.append(len_ + i)
+
+        agg.drop(agg.columns[col_numbers_drop], axis=1, inplace=True)
+        return agg
+
+
+if __name__ == '__main__':
+    # get the code data from websit
+    codes = prd.get_codes(STOCK_INFO_FILE)
+    startdate = str(codes[0]).replace('\n', '')  # 回测开始时间
+    enddate = str(codes[1]).replace('\n', '')  # 回测结束时间
+    it = 0
+    code = ""
+    filepath = ""
+    for code in codes:
+        if it > 1:
+            code = str(codes[it]).replace('\n', '')  # "sz300598"
+            filepath = prd.prepare_data(code, startdate, enddate)
+        it += 1
+    df = prd.load_data(filepath)
+    #  test stock file
+    #    filepath = 'C:\\01 Work\\13 program\PyProject1.0\stocks\sz300589.csv'
+    #    df = prd.load_data(filepath)
+    p_df = prd.pure_data(df, None)
+    pre_deal = PreTreadData(p_df)
+    all_data_set = p_df.copy()  # date is index and already setted in func load_data()
+
+    len_ = len(['open', 'high', 'low', 'close']) * 3
+    col_numbers_drop = []
+    for i in range(3):
+        col_numbers_drop.append(len_ + i)
+    print(col_numbers_drop)
+
+    #all_data_set2["Index"] = pd.to_datetime(all_data_set2["Index"])  # 日期object: to datetime
+    #all_data_set2.set_index("Index", inplace=True, drop=True)  # 把index设为索引
+    #all_data_set2 = all_data_set2[116:]  # 这里把7月28日的数据全部删掉了，主要是数据缺失较多
+
+    data_set_process = pre_deal.series_to_supervised(all_data_set, 500)  # 取近10分钟的数据
+    print(data_set_process.columns.values)
+    print(data_set_process.info())
