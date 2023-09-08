3d7a7346f719dcf6714e319da093343c4b59bb18
liuliu <183741946@qq.com>
2023-09-08 12:47:41 +0800

    2023/09/06: 1.参数优化

--- MyStrategy/XGBoostModelTest.py
+++ MyStrategy/XGBoostModelTest.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; py-indent-offset:4 -*-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from sklearn.preprocessing import MinMaxScaler
 import xgboost as xgb
 from matplotlib import pyplot as plt
 import numpy as np
 from xgboost import plot_importance, plot_tree
 
 
-def xgb_train(df):
+def xgb_train(df, rundnum, time_windows):
     data_set_process = df
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data_set_process)
+
     train_size = int(len(data_set_process)*0.8)
     test_size = len(data_set_process) - train_size
-    train_XGB, test_XGB = scaled_data[0:train_size,:], scaled_data[train_size:len(data_set_process),:]
+    train_XGB, test_XGB = scaled_data[0:train_size, :], scaled_data[train_size:len(data_set_process), :]
 
-    train_XGB_X, train_XGB_Y = train_XGB[:, :(len(data_set_process.columns)-1)], train_XGB[:,(len(data_set_process.columns)-1)]
-    test_XGB_X, test_XGB_Y = test_XGB[:, :(len(data_set_process.columns)-1)], test_XGB[:,(len(data_set_process.columns)-1)]
+    train_XGB_X, train_XGB_Y = \
+        train_XGB[:, :(len(data_set_process.columns)-1)], train_XGB[:, (len(data_set_process.columns)-1)]
+    test_XGB_X, test_XGB_Y = \
+        test_XGB[:, :(len(data_set_process.columns)-1)], test_XGB[:, (len(data_set_process.columns)-1)]
 
+# 'objective': 'binary:logistic',  # 此处为回归预测，这里如果改成multi:softmax 则可以进行多分类
     # 算法参数
     params = {
         'booster': 'gbtree',
-        'objective': 'binary:logistic',  # 此处为回归预测，这里如果改成multi:softmax 则可以进行多分类
+        'objective': 'binary:logistic',
         'gamma': 0.1,
         'max_depth': 5,
         'lambda': 3,
@@ -34,8 +43,8 @@ def xgb_train(df):
     #生成数据集格式
     xgb_train = xgb.DMatrix(train_XGB_X, label=train_XGB_Y)
     xgb_test = xgb.DMatrix(test_XGB_X, label=test_XGB_Y)
-    num_rounds = 300
-    watchlist = [(xgb_test, 'eval'),(xgb_train, 'train')]
+    num_rounds = rundnum
+    watchlist = [(xgb_test, 'eval'), (xgb_train, 'train')]
 
     #xgboost模型训练
     model_xgb = xgb.train(params, xgb_train, num_rounds, watchlist)
@@ -46,14 +55,11 @@ def xgb_train(df):
     # 模型结果可视化及评估
     plt.plot(test_XGB_Y, color='red', label='Real Price for Test set')
     plt.plot(y_pred_xgb, color='blue', label='Predicted Price for Test set')
-    plt.title('Zclose Price Prediction for Test set')
+    plt.title(f'Close Price Prediction for Test set: {time_windows}')
     plt.xlabel('Time')
-    plt.ylabel('Sohu Zclose Price')
+    plt.ylabel('Close Price')
     plt.legend()
     plt.show()
 
     mape_xgb = np.mean(np.abs(y_pred_xgb-test_XGB_Y)/test_XGB_Y)*100
     print('XGBoost平均误差率为：{}%'.format(mape_xgb))  #平均误差率为1.1974%
-
-
-
